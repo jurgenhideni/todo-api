dc82e6846eb4d2c6712db0d60c376ed04fee8f64
jurgenhideni97 <hidenijurgen@gmail.com>
2023-03-12 11:46:44 +0100

    Created credentials middleware

--- src/server.js
+++ src/server.js
@@ -1,24 +1,26 @@
-require('dotenv').config({ path: '.env.local' })
+require("dotenv").config({ path: ".env.local" });
 
-const cors = require('cors')
-const express = require('express')
-const { connection } = require('mongoose')
+const cors = require("cors");
+const express = require("express");
+const { connection } = require("mongoose");
 
-const connectDB = require('./config/connectDB')
-const corsOptions = require('./config/corsOptions')
+const connectDB = require("./config/connectDB");
+const corsOptions = require("./config/corsOptions");
+const credentials = require("./middlewares/credentials");
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use('/todos', require('./routes/todos'))
+app.use(credentials);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use("/todos", require("./routes/todos"));
 
-connection.once('open', () => {
-    console.log('Database connected!')
+connection.once("open", () => {
+  console.log("Database connected!");
 
-    app.listen(4000, () => {
-        console.log(`Server started!`)
-    })
-})
+  app.listen(4000, () => {
+    console.log(`Server started!`);
+  });
+});
